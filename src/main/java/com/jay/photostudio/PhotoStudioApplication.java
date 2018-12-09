7b36430e9a31a08b35f9849464378af477ac2d40
yuefanjie@163.com <331545911Yfj>
2018-12-10 00:45:38 +0800

    start

---
+++ src/main/java/com/jay/photostudio/PhotoStudioApplication.java
@@ -0,0 +1,12 @@
+package com.jay.photostudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoStudioApplication.class, args);
+    }
+}
